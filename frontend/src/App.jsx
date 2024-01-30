1a17aa64f3d74a333d3ce51bcf1a56aa3509e821
DoBleh <ilhamdile05@gmail.com>
2024-01-30 12:50:37 +0700

    jkcask

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 
 import Navbar from "./components/navbar";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<h1>hello World!</h1>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+      
     </>
   );
 }
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -99,7 +99,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/buku"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
